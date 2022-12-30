e4ce8658430adc52a2af9027be547ce9153f8b63
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2022-12-30 15:32:44 -0600

    Transform getComputerChoice into function

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 
 let userInput;
-const rps = ["rock", "paper", "scissors"];
 
-const getComputerChoice = rps[(Math.random() * rps.length) | 0];
-console.log(getComputerChoice);
+function getComputerChoice() {
+    const rps = ["rock", "paper", "scissors"];
+    return rps[Math.floor(Math.random() * rps.length)];
+}
+console.log(getComputerChoice());
+  // Computer randomly selects "rock," "paper," or "scissors" as its response
 
+/* rock is < paper;
+paper < scissors;
+scissors < rock; 
+let userInput ["rock", "paper", or "scissors"];
+let getComputerChoice = random("rock", "paper", or "scissors");
+if (userInput < getComputerChoice) {
+    return "You lose.";
+}
+else if (userInput > getComputerChoice) {
+    return "You win.";
+}
+*/
+
+// let playerText = "scissors";
+// let playerSelection = playerText.toLowerCase(); 
+// let computerSelection = getComputerChoice;
+
+// function singleRound(playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//         return "tie.";
+//     }
+//     else if (playerSelection === "rock" && computerSelection === "paper") {
+//         return "you lose.";
+//     }
+//     else if (playerSelection === "rock" && computerSelection === "scissors") {
+//         return "you win.";
+//     }
+//     else if (playerSelection === "paper" && computerSelection === "scissors") {
+//         return "you lose.";
+//     }
+//     else if (playerSelection === "paper" && computerSelection === "rock") {
+//         return "you win.";
+//     }
+//     else if (playerSelection === "scissors" && computerSelection === "rock") {
+//         return "you lose.";
+//     }
+//     else if (playerSelection === "scissors" && computerSelection === "paper") {
+//         return "you win.";
+//     }
+//     else if (playerSelection === "rock" && computerSelection === "paper") {
+//         return "you lose.";
+//     }
+// }
+// console.log(singleRound(playerSelection, computerSelection));
 
 
 
