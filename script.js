021e28d4cf8e53fd0b20dd018e7012c09b8ecffe
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2022-12-30 17:56:51 -0600

    Add score keeping for best of 5 gameplay rounds

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-let userInput;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const rps = ["rock", "paper", "scissors"];
@@ -19,24 +20,31 @@ if (playerSelection === computerSelection) {
     return "tie.";
 }
 else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++
     return "you lose. paper beats rock.";
 }
 else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++
     return "you win. rock beats scissors.";
 }
 else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++
     return "you lose. scissors beats paper.";
 }
 else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++
     return "you win. paper beats rock.";
 }
 else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++
     return "you lose. rock beats scissors.";
 }
 else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++
     return "you win. paper beats scissors.";
 }
 }
+
 
 
 console.log(playRound(playerSelection, computerSelection));
@@ -49,9 +57,18 @@ function game(){
         console.log(playRound(playerSelection, computerSelection));
         
     }
+    if (playerScore > computerScore) {
+        return "You won best of 5.";
+    }
+    else if (playerScore < computerScore) {
+        return "You lost best of 5."
+    }
+    else {
+        return "You tied best of 5."
+    }
     }
     
-game();
+console.log(game());
 
 
 
