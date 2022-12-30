748ff20764e74906b6cd1e400f1275893eb16cea
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2022-12-30 12:03:03 -0600

    Rewrite getComputerChoice to randomly select "rock," "paper," or "scissors"

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 
 let userInput;
+const rps = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    if (userInput === 1) {
-        return "rock";
-    }
-    else if (userInput === 2) {
-        return "paper";
-    }
-    else if (userInput === 3) {
-        return "scissors";
-    }
-    else {
-        return "Choose a number from 1 to 3"
-    }
-}
-console.log(getComputerChoice());
+const getComputerChoice = Math.floor(Math.random() * rps.length);
+console.log(getComputerChoice, rps[getComputerChoice]);
+
+
 
 
