2084503cc2d7acfd1755d09e9b588c34448030fd
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-18 06:46:22 -0500

    Add best of five message.

--- script.js
+++ script.js
@@ -1,76 +1,110 @@
-
+let rbtn = document.querySelector('#rock');
+let pbtn = document.querySelector('#paper');
+let sbtn = document.querySelector('#scissors');
+let resultsdiv = document.querySelector('#results');
 let playerScore = 0;
 let computerScore = 0;
+let computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     const rps = ["rock", "paper", "scissors"];
     return rps[Math.floor(Math.random() * rps.length)];
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
   // Computer randomly selects "rock," "paper," or "scissors" as its response
 
-
-let playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase();
-let computerSelection = getComputerChoice();
-
+  
+// let playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase();
 
 
 function playRound(playerSelection, computerSelection) {
-if (playerSelection === computerSelection) {
-    return "tie.";
+   getComputerChoice();
+   if (playerSelection === computerSelection) {
+   const p = document.createElement('p');
+   // p.classList.add('');
+   p.textContent = "tie.";
+   resultsdiv.appendChild(p);
 }
 else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++
-    return "you lose. paper beats rock.";
+    const p = document.createElement('p');
+    p.textContent = "you lose. paper beats rock.";
+    resultsdiv.appendChild(p);
 }
 else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++
-    return "you win. rock beats scissors.";
+    const p = document.createElement('p'); 
+    p.textContent = "you win. rock beats scissors.";
+    resultsdiv.appendChild(p);
 }
 else if (playerSelection === "paper" && computerSelection === "scissors") {
     computerScore++
-    return "you lose. scissors beats paper.";
+    const p = document.createElement('p');
+    p.textContent = "you lose. scissors beats paper.";
+    resultsdiv.appendChild(p);
 }
 else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++
-    return "you win. paper beats rock.";
+    const p = document.createElement('p');
+    p.textContent = "you win. paper beats rock.";
+    resultsdiv.appendChild(p);
 }
 else if (playerSelection === "scissors" && computerSelection === "rock") {
     computerScore++
-    return "you lose. rock beats scissors.";
+    const p = document.createElement('p');
+    p.textContent = "you lose. rock beats scissors.";
+    resultsdiv.appendChild(p);
 }
 else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++
-    return "you win. paper beats scissors.";
+    const p = document.createElement('p');
+    p.textContent = "you win. paper beats scissors.";
+    resultsdiv.appendChild(p);
 }
 }
-
-
-
-console.log(playRound(playerSelection, computerSelection));
+checkForWinner = (playerScore, computerScore) => {
+    if (playerScore === 5) {
+        const p = document.createElement('p');
+        p.textContent = "You win best of 5!";
+        resultsdiv.appendChild(p);
+    } else if (computerScore === 5) {
+        const p = document.createElement('p');
+        p.textContent = "Computer wins, best of 5.";
+        resultsdiv.appendChild(p);
+    }
+    }
 
 function game(){
 
-    for (let i = 0; i < 5; i++) {
+   // for (let i = 0; i < 5; i++) {
         let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
+       // let playerSelection = prompt("Rock, paper, or scissors?", "").toLowerCase();
+        playRound(playerSelection, computerSelection);
         
     }
-    // Loops programme 5 times
     
-    if (playerScore > computerScore) {
-        return "You won best of 5.";
-    }
-    else if (playerScore < computerScore) {
-        return "You lost best of 5."
-    }
-    else {
-        return "You tied best of 5."
-    }
-    }
-    
-console.log(game());
+
+rbtn.addEventListener('click', function(e) {
+    playRound('rock', computerSelection);
+    checkForWinner(playerScore, computerScore);
+    }); 
+
+pbtn.addEventListener('click', function(e) {
+    playRound('paper', computerSelection);
+    checkForWinner(playerScore, computerScore);
+    });
+
+sbtn.addEventListener('click', function(e) {
+    playRound('scissors', computerSelection);
+    checkForWinner(playerScore, computerScore);
+    });
+
+
+// console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 
 
 
